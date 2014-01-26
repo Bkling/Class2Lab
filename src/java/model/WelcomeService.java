9def8e62436188b62fa65bab429ef915e3dbe745
benja_000 <benja_000@Benjamin-Asus>
2014-01-26 16:22:43 -0600

    Working on Lab 4.

--- src/java/model/WelcomeService.java
+++ src/java/model/WelcomeService.java
@@ -3,13 +3,41 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package model;
+
+import java.util.Calendar;
 
 /**
  *
  * @author benja_000
  */
 public class WelcomeService {
-    
+
+    private Calendar cal = Calendar.getInstance();
+    private String name;
+
+    public WelcomeService(String name) {
+        this.name = name;
+    }
+
+    public WelcomeService() {
+
+    }
+
+    public Calendar getCal() {
+        return cal;
+    }
+
+    public void setCal(Calendar cal) {
+        this.cal = cal;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
